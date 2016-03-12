5b435ff6ca0d1c367fbd075324bdd972276e680f
Sung Won Cho <mikeswcho@gmail.com>
2016-03-12 14:00:53 +1100

    Implement validator for naming convention

--- test/validator_test.js
+++ test/validator_test.js
@@ -1,8 +1,11 @@
 import path from 'path';
+import fse from 'fs-extra';
 import {expect} from 'chai';
 import {prepareDummyApp, clearDummyApp} from './helpers';
 
-import {validateDirStructure} from '../lib/validators/dir_structure';
+import {
+  validateDirStructure, validateNamingConvention
+} from '../lib/validators';
 
 const dummyAppPath = path.resolve(__dirname, '../tmp/dummy');
 
@@ -32,6 +35,43 @@ describe("validators", function() {
       });
       let result = validateDirStructure(dummyAppPath);
       expect(result.status).to.equal('failing');
+    });
+  });
+
+  describe("naming_convention", function() {
+    it("returns successful status if no validation is found", function() {
+      prepareDummyApp({appPath: dummyAppPath});
+      let result = validateNamingConvention(dummyAppPath);
+      expect(result.status).to.equal('successful');
+    });
+
+    it("returns failing status if a filename includes a dash", function() {
+      prepareDummyApp({appPath: dummyAppPath});
+      fse.outputFileSync(`${dummyAppPath}/client/modules/core/containers/tests/main-header.js`);
+      let result = validateNamingConvention(dummyAppPath);
+      expect(result.status).to.equal('failing');
+    });
+
+    it("returns failing status if a filename includes a dot", function() {
+      prepareDummyApp({appPath: dummyAppPath});
+      fse.outputFileSync(`${dummyAppPath}/client/modules/core/containers/main.layout.js`);
+      let result = validateNamingConvention(dummyAppPath);
+      expect(result.status).to.equal('failing');
+    });
+
+    it("returns failing status if there is no matching filename for a test filename", function() {
+      prepareDummyApp({appPath: dummyAppPath});
+      fse.outputFileSync(`${dummyAppPath}/client/modules/core/containers/tests/comment_list.js`);
+      let result = validateNamingConvention(dummyAppPath);
+      expect(result.status).to.equal('failing');
+    });
+
+    it("returns successful status if there is a matching filename for a test filename", function() {
+      prepareDummyApp({appPath: dummyAppPath});
+      fse.outputFileSync(`${dummyAppPath}/client/modules/core/containers/tests/comment_list.js`);
+      fse.outputFileSync(`${dummyAppPath}/client/modules/core/containers/comment_list.js`);
+      let result = validateNamingConvention(dummyAppPath);
+      expect(result.status).to.equal('successful');
     });
   });
 });
