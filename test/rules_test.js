d78d23b64925097a395784b6bb7c98fa8868df76
Sung Won Cho <mikeswcho@gmail.com>
2016-03-12 19:10:42 +1100

    Implement container rule

--- test/rules_test.js
+++ test/rules_test.js
@@ -1,10 +1,11 @@
 import path from 'path';
+import fs from 'fs';
 import fse from 'fs-extra';
 import {expect} from 'chai';
 import {prepareDummyApp, clearDummyApp} from './helpers';
 
 import {
-  lintDirStructure, lintNamingConvention
+  lintDirStructure, lintNamingConvention, lintContainer
 } from '../lib/rules';
 
 const dummyAppPath = path.resolve(__dirname, '../tmp/dummy');
@@ -72,6 +73,61 @@ describe("rules", function() {
       fse.outputFileSync(`${dummyAppPath}/client/modules/core/containers/comment_list.js`);
       let result = lintNamingConvention(dummyAppPath);
       expect(result.status).to.equal('passing');
+    });
+  });
+
+  describe("containers", function() {
+    it("passes if no violation is found", function() {
+      prepareDummyApp({appPath: dummyAppPath});
+      let containerPath = `${dummyAppPath}/client/modules/core/containers/post_list.js`;
+      fse.copySync(__dirname + '/fixtures/container.tt', containerPath);
+      let result = lintContainer(containerPath);
+      expect(result.status).to.equal('passing');
+    });
+
+    it("fails if the composer is not exported", function() {
+      let containerCode = fs.readFileSync(`${__dirname}/fixtures/container.tt`, {encoding: 'utf-8'});
+      let invalidCode = containerCode.replace('export const composer', 'const composer');
+      let containerPath = `${dummyAppPath}/client/modules/core/containers/post_list.js`;
+      fse.outputFileSync(containerPath, invalidCode);
+
+      let result = lintContainer(containerPath);
+      expect(result.status).to.equal('failing');
+    });
+
+    it("failes if the container is not exported by default", function() {
+      let containerCode = fs.readFileSync(`${__dirname}/fixtures/container.tt`, {encoding: 'utf-8'});
+      let invalidCode = containerCode.replace('export default composeAll(', 'function composeAll(');
+      let containerPath = `${dummyAppPath}/client/modules/core/containers/post_list.js`;
+      fse.outputFileSync(containerPath, invalidCode);
+
+      let result = lintContainer(containerPath);
+      expect(result.status).to.equal('failing');
+    });
+
+    it("failes if a mapper is defined but not exported", function() {
+      let invalidCode =
+`import PostList from '../components/post_list.jsx';
+import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
+
+export const composer = ({context}, onData) => {
+  const {Meteor, Collections} = context();
+};
+
+const depsMapper = (context, actions) => ({
+  context: () => context
+});
+
+export default composeAll(
+  composeWithTracker(composer),
+  useDeps(depsMapper)
+)(PostList);
+`;
+      let containerPath = `${dummyAppPath}/client/modules/core/containers/post_list.js`;
+      fse.outputFileSync(containerPath, invalidCode);
+
+      let result = lintContainer(containerPath);
+      expect(result.status).to.equal('failing');
     });
   });
 });
